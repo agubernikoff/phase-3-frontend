faa9756d7d9273b397976fd4dc7ac00186fff9c8
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-04-01 13:13:43 -0400

    pushing finalish version for project presentation

--- src/App.js
+++ src/App.js
@@ -13,22 +13,19 @@ import {
   ViewSwitcher,
   //IntegratedEditing,
   EditRecurrenceMenu,
-  AppointmentTooltip,
+  //AppointmentTooltip,
   AppointmentForm,
   DragDropProvider,
   ConfirmationDialog,
 } from "@devexpress/dx-react-scheduler-material-ui";
 import "./App.css";
 import DateTime from "./DateTime";
-import { colors } from "@mui/material";
-import { RRule, RRuleSet, rrulestr } from "rrule";
 import { style } from "@mui/system";
 import PriorityGuide from "./PriorityGuide";
 
 // Get current date and format it correctly (YYYY-MM-DD)
 const date = new Date();
 function formatMonth(month) {
-  console.log(month);
   if (month > 10) return month;
   else return "0" + month;
 }
@@ -36,28 +33,6 @@ const currentDate = `${date.getFullYear()}-${formatMonth(
   date.getMonth() + 1
 )}-${date.getDate()}`;
 
-// const schedulerData = [
-//   {
-//     startDate: "2018-11-01T09:45",
-//     endDate: "2018-11-01T11:00",
-//     title: "Meeting",
-//   },
-//   {
-//     startDate: "2018-11-01T12:00",
-//     endDate: "2018-11-01T13:30",
-//     title: "Go to the gym",
-//   },
-//   {
-//     startDate: "2022-04-01T12:00",
-//     endDate: "2022-04-01T13:30",
-//     title: "Finish the Project",
-//   },
-//   {
-//     startDate: "2022-03-31T22:00",
-//     endDate: "2022-04-01T08:00",
-//     title: "Sleep",
-//   },
-// ];
 function renderCategory(category) {
   switch (category) {
     case 1:
@@ -79,11 +54,9 @@ function renderCategory(category) {
   }
 }
 function formatAppt(json) {
-  console.log("json passed to formatappt:", json);
   let formattedArr = [];
   for (let i = 0; i < json.length; i++) {
     for (let j = 0; j < json[i].task_times.length; j++) {
-      console.log("in inner for loop");
       const objToPush = {
         startDate: json[i].task_times[j].startDate,
         endDate: json[i].task_times[j].endDate,
@@ -91,35 +64,16 @@ function formatAppt(json) {
         id: json[i].task_times[j].id,
         allDay: !!json[i].task_times[j].allDay,
         task_def_id: json[i].id,
-        //"[propertyName: 'category:']": json.task_def.category,
         priority: json[i].priority_id,
         rRule: json[i].rRule,
+        exDate: json[i].exDate,
         category: json[i].category_id,
         description: json[i].description,
       };
-      console.log("objToPush:", objToPush);
       formattedArr.push(objToPush);
     }
   }
-  console.log("formatted array!!!!!", formattedArr);
   return formattedArr;
-}
-
-function format1Appt(json) {
-  const objToPush = {
-    startDate: json.task_times[0].startDate,
-    endDate: json.task_times[0].endDate,
-    title: json.title,
-    id: json.task_times[0].id,
-    allDay: !!json.task_times[0].allDay,
-    task_def_id: json.id,
-    //"[propertyName: 'category:']": json.task_def.category,
-    priority: json.priority_id,
-    rRule: json.rRule,
-    category: json.category_id,
-    description: json.description,
-  };
-  return objToPush;
 }
 
 function App() {
@@ -131,57 +85,21 @@ function App() {
       .then((r) => r.json())
       .then((tasks) => {
         const formatArr = formatAppt(tasks);
-        console.log("formatAArr:", formatArr);
         setTaskTimes(formatArr);
       });
   }, []);
 
   function onEdit(e) {
-    console.log(e);
     //call function to update an existing appointment
-    if (Object.keys(e)[0] === "changed") {
-      console.log("yay changed");
+    if (Object.keys(e).includes("changed")) {
       patchAppt(e.changed);
     }
     //call function to delete appointment
     if (Object.keys(e)[0] === "deleted") {
-      console.log(e.deleted);
-      //commented out because this function isn't complete or tested
       deleteAppt(e.deleted);
     }
     //call function to post new appointment
-    if (Object.keys(e)[0] === "added") {
-      // if (e.added.rRule) {
-      //   console.log(e.added);
-      //   //invoke rrulstr on correctly formatted startDate + rRule to create a new rRule obj.
-      //   //invoke .all() to get every instance of the recurring event.
-      //   // map over that to correctly format the date.
-      //   // map over that to return an appropriately formatted task object
-      //   // CHECK CONSOLE AFTER CREATING A NEW TASK TO SEE RESULTS
-      //   const recurringDates = rrulestr(
-      //     `DTSTART:${e.added.startDate
-      //       .toISOString()
-      //       .replace(/[^a-zA-Z0-9 ]/g, "")
-      //       .slice(0, -4)}Z\n${e.added.rRule}`.toString()
-      //   )
-      //     .all()
-      //     .map((date) => date.toISOString())
-      //     .map((date) => ({
-      //       startDate: date,
-      //       endDate:
-      //         date.slice(0, 11) + e.added.endDate.toISOString().slice(11),
-      //       title: e.added.title,
-      //       // allDay: e.added.allDay,
-      //       // rRule: e.added.rRule,
-      //     }));
-      //   const objToPost = { ...e.added, recurringDates: recurringDates };
-      //   postAppt(objToPost);
-      //   // for (let i = 0; i < recurringDates.length; i++) {
-      //   //   postAppt(recurringDates[i]);
-      //   // }
-      //   // setTimeout(() => setTaskTimes([...taskTimes, ...recurringDates]), 300);
-      // }
-      //commented out because this function isn't complete or tested
+    if (Object.keys(e).includes("added")) {
       postAppt(e.added);
     }
   }
@@ -199,8 +117,6 @@ function App() {
 
   function patchAppt(eObj) {
     const id = Object.keys(eObj)[0];
-    console.log(eObj[id]);
-    console.log(id);
     fetch(`http://localhost:9292/task_times/${id}`, {
       method: "PATCH",
       headers: {
@@ -210,13 +126,10 @@ function App() {
     })
       .then((r) => r.json())
       .then((json) => {
-        const updated = format1Appt(json);
-        console.log("updated:", updated);
+        const updated = formatAppt([json]);
         const updatedArr = taskTimes.map((task) => {
-          console.log("taskid=", task.id, "id=", id);
           if (task.id == id) {
-            console.log("in if???");
-            return updated;
+            return updated[0];
           }
           return task;
         });
@@ -225,7 +138,6 @@ function App() {
   }
 
   function postAppt(eObj) {
-    console.log(eObj);
     fetch("http://localhost:9292/task_times", {
       method: "POST",
       headers: {
@@ -236,11 +148,9 @@ function App() {
       .then((r) => r.json())
       .then((json) => {
         const newAppt = formatAppt([json]);
-        console.log("post new appt return", newAppt);
         setTaskTimes([...taskTimes, ...newAppt]);
       });
   }
-  console.log(taskTimes);
 
   function renderPriority(priority) {
     switch (priority) {
@@ -319,7 +229,7 @@ function App() {
           <Appointments appointmentComponent={Appointment} />
           <EditingState onCommitChanges={onEdit} />
           <EditRecurrenceMenu />
-          <AppointmentTooltip showOpenButton />
+          {/* <AppointmentTooltip showOpenButton /> */}
           <AppointmentForm />
           <DragDropProvider />
           <ConfirmationDialog />
